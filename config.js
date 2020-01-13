3ad8ebbb1ca56cc1d791c123c7a6bdf9c5c36d80
TeaSongFugue <59817772+TeaSongFugue@users.noreply.github.com>
2020-01-13 16:02:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -23,17 +23,23 @@ var config = {
      */
     imgs: {
         "送给亲爱的娃": "./imgs/xiaochuan.jpg",
+        "祝你生日快乐！": "./imgs/xiaochuan2.jpg",
+        "在三字头开始的日子里": "./imgs/xiaochuan2.jpg",
+        "把头发剪成年轻模样": "./imgs/xiaochuan5.jpg",
+        "穿上一身幼稚裙装": "./imgs/xiaochuan6.jpg",
+        "FOREVER YOUNG": "./imgs/xiaochuan7.jpg",
+        
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "TURN ON",
+        play: "PLAY",
+        bannar_coming: "COLOR",
+        balloons_flying: "BALLOON?",
+        cake_fadein: "CAKE?",
+        light_candle: "CANDLE?",
+        wish_message: "HAPPY BIRTHDAY!",
         story: "A MESSAGE FOR YOU",
     }
 };
